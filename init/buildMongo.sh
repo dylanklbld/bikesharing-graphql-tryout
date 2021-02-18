f45016f28e44085a0e93e4bec29197eea17ae325
Danil Minibaev <d.minibaev@epages.com>
2021-02-18 10:36:50 +0300

    Fix: Run srcipts

--- init/buildMongo.sh
+++ init/buildMongo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec mongo-on-docker mongo rent-bikes  -u mongoadmin -p secret --authenticationDatabase admin --eval 'db.createUser({ user: "mongoadmin", pwd: "secret", roles: [{ role: "readWrite", db: "rent-bikes" }]})' 
+docker exec mongo-on-docker mongo rent-bikes  -u mongoadmin -p secret --authenticationDatabase admin --eval '!db.runCommand({ usersInfo: { user: "mongoadmin", db: "rent-bikes" } }).users.length == 1 && db.createUser({ user: "mongoadmin", pwd: "secret", roles: [{ role: "readWrite", db: "rent-bikes" }]})' 
